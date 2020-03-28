6c4973bb71dcc13adacda9771e0e47d9365b1124
dendiod <49756950+dendiod@users.noreply.github.com>
2020-03-28 16:38:34 +0300

    Update getId.js

--- public/getId.js
+++ public/getId.js
@@ -2,6 +2,10 @@ var id;
 let user = JSON.stringify({message: 'send me id'});			
  let request = new XMLHttpRequest();
   request.open("POST", "/id", true);   
+  request.onerror = function () {
+	alert("Connection with server failed");
+	return;
+  };
   request.setRequestHeader("Content-Type", "application/json");
   request.addEventListener("load", function () {
       let receivedUser = JSON.parse(request.response);
